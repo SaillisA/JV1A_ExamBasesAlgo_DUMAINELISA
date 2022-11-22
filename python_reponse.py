862454972673bbe62ec113069343165cf9565f47
SaillisA <dumaine.lisa04@gmail.com>
2022-11-22 13:15:12 +0100

    petit 1 permuter

--- python_reponse.py
+++ python_reponse.py
@@ -1,1 +1,9 @@
-import random
+#1
+myTable=[1,2,3,4]
+def permuter(tab,indice1,indice2):
+    var_tempo=tab[indice1]
+    tab[indice1]=tab[indice2]
+    tab[indice2]=var_tempo
+    return myTable
+
+print(permuter(myTable,2,1))

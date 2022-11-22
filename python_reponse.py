b57969c647a08e17ca52d4956b4973448bec3ed3
SaillisA <dumaine.lisa04@gmail.com>
2022-11-22 13:16:30 +0100

    description 1.

--- python_reponse.py
+++ python_reponse.py
@@ -1,9 +1,11 @@
 #1
 myTable=[1,2,3,4]
+
+
 def permuter(tab,indice1,indice2):
-    var_tempo=tab[indice1]
-    tab[indice1]=tab[indice2]
-    tab[indice2]=var_tempo
-    return myTable
+    var_tempo=tab[indice1]              #on stocke la variable pour la perdre
+    tab[indice1]=tab[indice2]           #puis on permute
+    tab[indice2]=var_tempo              #et on recupere la variable qu'on avait stocké
+    return myTable                      
 
 print(permuter(myTable,2,1))
